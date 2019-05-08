044d93e342456e907336653ca82860c80053e5c7
Infinical <inficalvin@gmail.com>
2019-05-08 11:35:15 +0300

    Create categories (#12)

--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,17 @@
 # It's strongly recommended that you check this file into your version control system.
 
 
-ActiveRecord::Schema.define(version: 20_190_425_085_456) do
+ActiveRecord::Schema.define(version: 2019_05_08_051534) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
+  create_table "categories", force: :cascade do |t|
+    t.string "title"
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 
   create_table "categories", force: :cascade do |t|
     t.string "title"
@@ -31,6 +39,7 @@ ActiveRecord::Schema.define(version: 20_190_425_085_456) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
 
   create_table "users", force: :cascade do |t|
     t.string "provider", default: "email", null: false
